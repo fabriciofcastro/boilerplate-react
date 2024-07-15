6ede7d5033f5846832cc3da3344af83c5c0ee948
Fabricio Castro <fabriciofeitoscastro@gmail.com>
2024-07-15 17:49:35 -0300

    defined routing with react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './components/routes/routes'
 
 export function App() {
-  return (
-    <>
-      <div className="flex justify-center items-center h-screen">
-        <Button>Enviar</Button>
-        <Button variant={'destructive'}>Enviar</Button>
-      </div>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
